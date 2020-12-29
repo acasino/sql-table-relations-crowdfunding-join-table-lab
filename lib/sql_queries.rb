34a302c7f403ce50d5d5460173646c7499b5a236
Andrew <andrew.casin@gmail.com>
2020-12-29 01:52:22 -0600

    commit tree 55d26da58b5e4d701264df08fec2fa6451a27506

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,24 +8,60 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "Write your SQL query Here"
+  "SELECT p1.title, SUM(p2.amount)
+  FROM projects p1
+  JOIN pledges p2
+  ON p1.id = p2.project_id
+  GROUP BY 1;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "Write your SQL query Here"
+  "SELECT p1.name, p1.age, SUM(p2.amount)
+  FROM users p1
+  JOIN pledges p2
+  ON p1.id = p2.user_id
+  GROUP BY 1;
+  "
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "Write your SQL query Here"
+  "SELECT p1.title, (SUM(p2.amount) - p1.funding_goal) AS remaining
+  FROM projects p1
+  JOIN pledges p2
+  ON p1.id = p2.project_id
+  GROUP BY p1.title HAVING remaining >= 0;
+  "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "Write your SQL query Here"
+  "SELECT p1.name, SUM(p2.amount)
+  FROM users p1
+  JOIN pledges p2
+  ON p1.id = p2.user_id
+  GROUP BY p1.name
+  ORDER BY SUM(p2.amount);
+  "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "Write your SQL query Here"
+  "SELECT p1.category, p2.amount
+  FROM projects p1
+  JOIN pledges p2
+  ON p1.id = p2.project_id
+  WHERE p1.category = 'music';
+  "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
   "Write your SQL query Here"
+  "SELECT p1.category, SUM(p2.amount)
+  FROM projects p1
+  JOIN pledges p2
+  ON p1.id = p2.project_id
+  WHERE p1.category = 'books';
+  "
 end
